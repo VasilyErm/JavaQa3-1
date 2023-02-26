1c310d38f57565112bdbb7d2c69595c897fae2e1
Vasily Ermolaev <vasily.erm@gmail.com>
2023-02-26 18:39:21 +0300

    Initional commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15000;
+        int bonus = price / 20;
+        System.out.println("Стоимость билета: " + price +(" рублей.") + " Вам будет начисленно: " + bonus + (" бонусных миль."));
+    }
+}
