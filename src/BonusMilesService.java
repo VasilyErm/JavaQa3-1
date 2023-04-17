44624cbbbaef8b59f09b4356d41056a640da9157
Vasily Ermolaev <vasily.erm@gmail.com>
2023-04-17 21:09:04 +0300

    Initional commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price){
-        int Bonus = price / 20;
-        return Bonus;
+        int bonus = price / 20;
+        return bonus;
     }
 }
